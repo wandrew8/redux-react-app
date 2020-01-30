fdcfbd4eaa8215ff957fabe2a0a2ffffd224306c
Andrew Weiss <wandrew8@gmail.com>
2020-01-30 13:55:31 -0800

    comment component added

---
+++ react-redux/client/components/Comments.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Comments = React.createClass({
+    renderComments(comment, i) {
+        return (
+            <div className="comment" key={i}>
+                <p>
+                <strong>{comment.user}</strong>
+                {comment.text}
+                <button className="remove-comment">&times;</button>
+                </p>
+            </div>
+        )
+    },
+    render() {
+        return(
+            <div className="comment" >
+                {this.props.comments.map(this.renderComments)}
+                <form ref="commentForm" className="comment-form">
+                    <input type="text" ref="author" placeholder="author"/>
+                    <input type="text" ref="comment" placeholder="comment"/>
+                    <input type="submit" hidden />
+                </form>
+            </div>
+        )
+    }
+})
+
+export default Comments;
